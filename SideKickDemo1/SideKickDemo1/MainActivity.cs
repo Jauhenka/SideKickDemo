551b8712552ab49b2e1568ccae27393dd5c73165
Eugene Kamaev <jauhenka@gmail.com>
2019-04-15 14:34:17 +0300

    Setting up initial project and basic UI

---
+++ SideKickDemo1/SideKickDemo1/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace SideKickDemo1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView TimeTextView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Button turnBackTimeButton = FindViewById<Button>(Resource.Id.buttonTurnBackTime);
+            ImageView exitImageView = FindViewById<ImageView>(Resource.Id.imageViewExit);
+            Button menuButton = FindViewById<Button>(Resource.Id.buttonMenu);
+            TimeTextView = FindViewById<TextView>(Resource.Id.textViewTime);
+
+            turnBackTimeButton.Click += TurnBackTimeButton_Click;
+        }
+
+        private void TurnBackTimeButton_Click(object sender, System.EventArgs e)
+        {
+            TimeTextView.Text = "It's adventure time now!";
+        }
+    }
+}
